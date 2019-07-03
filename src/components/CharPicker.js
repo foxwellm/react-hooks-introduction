d005be8ddfb701d9467a02376c69b10b634d5499
Matthew Foxwell <foxwellmw@gmail.com>
2019-07-03 10:57:59 -0500

    Update CharPicker to hook, inc componentDidMount

--- src/components/CharPicker.js
+++ src/components/CharPicker.js
@@ -1,63 +1,113 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './CharPicker.css';
 
-class CharPicker extends Component {
-  state = { characters: [], isLoading: false };
+const CharPicker = (props) => {
+  // state = { characters: [], isLoading: false };
+  const [characters, setCharacters] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
 
-  componentDidMount() {
-    this.setState({ isLoading: true });
-    fetch('https://swapi.co/api/people')
-      .then(response => {
+  // componentDidMount() {
+  //   this.setState({ isLoading: true });
+  //   fetch('https://swapi.co/api/people')
+  //     .then(response => {
+  //       if (!response.ok) {
+  //         throw new Error('Failed to fetch.');
+  //       }
+  //       return response.json();
+  //     })
+  //     .then(charData => {
+  //       const selectedCharacters = charData.results.slice(0, 5);
+  //       this.setState({
+  //         characters: selectedCharacters.map((char, index) => ({
+  //           name: char.name,
+  //           id: index + 1
+  //         })),
+  //         isLoading: false
+  //       });
+  //     })
+  //     .catch(err => {
+  //       console.log(err);
+  //     });
+  // }
+
+  //useEffect runs after initial component has been rendered and after each time it rerenders
+  //second argument, array of dependencies that says when should update
+  //passing empty array, no data can change and so only runs once (componentDidMount)
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchStars = async () => {
+      try {
+        const response = await fetch('https://cors-anywhere.herokuapp.com/https://swapi.co/api/people')
         if (!response.ok) {
           throw new Error('Failed to fetch.');
         }
-        return response.json();
-      })
-      .then(charData => {
-        const selectedCharacters = charData.results.slice(0, 5);
-        this.setState({
-          characters: selectedCharacters.map((char, index) => ({
+        const data = await response.json()
+        const selectedCharacters = data.results.slice(0, 5);
+        setCharacters(
+          selectedCharacters.map((char, index) => ({
             name: char.name,
             id: index + 1
-          })),
-          isLoading: false
-        });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
-
-  render() {
-    let content = <p>Loading characters...</p>;
-
-    if (
-      !this.state.isLoading &&
-      this.state.characters &&
-      this.state.characters.length > 0
-    ) {
-      content = (
-        <select
-          onChange={this.props.onCharSelect}
-          value={this.props.selectedChar}
-          className={this.props.side}
-        >
-          {this.state.characters.map(char => (
-            <option key={char.id} value={char.id}>
-              {char.name}
-            </option>
-          ))}
-        </select>
-      );
-    } else if (
-      !this.state.isLoading &&
-      (!this.state.characters || this.state.characters.length === 0)
-    ) {
-      content = <p>Could not fetch any data.</p>;
+          }))
+        );
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+        setIsLoading(false);
+      }
     }
-    return content;
+    fetchStars()
+    // fetch('https://cors-anywhere.herokuapp.com/https://swapi.co/api/people')
+    //   .then(response => {
+    //     if (!response.ok) {
+    //       throw new Error('Failed to fetch.');
+    //     }
+    //     return response.json();
+    //   })
+    //   .then(charData => {
+    //     const selectedCharacters = charData.results.slice(0, 5);
+    //     setCharacters(
+    //       selectedCharacters.map((char, index) => ({
+    //         name: char.name,
+    //         id: index + 1
+    //       }))
+    //     );
+    //     setIsLoading(false);
+    //   })
+    //   .catch(err => {
+    //     console.log(err);
+    //     setIsLoading(false);
+    //   });
+  }, [])
+
+
+  let content = <p>Loading characters...</p>;
+
+  if (
+    !isLoading &&
+    characters &&
+    characters.length > 0
+  ) {
+    content = (
+      <select
+        onChange={props.onCharSelect}
+        value={props.selectedChar}
+        className={props.side}
+      >
+        {characters.map(char => (
+          <option key={char.id} value={char.id}>
+            {char.name}
+          </option>
+        ))}
+      </select>
+    );
+  } else if (
+    !isLoading &&
+    (!characters || characters.length === 0)
+  ) {
+    content = <p>Could not fetch any data.</p>;
   }
+  return content;
 }
 
 export default CharPicker;
